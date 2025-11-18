0d713a96dd55f3d5e7672c2319c3d61d7066209b
Shabreena Vincent <119475721+shabreenavincent@users.noreply.github.com>
2025-11-18 14:50:22 +0530

    Add files via upload

---
+++ L3_Image_generation_app.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # L3: Image generation app 🎨
+
+# Load your HF API key and relevant Python libraries
+
+# In[1]:
+
+
+import os
+import io
+import IPython.display
+from PIL import Image
+import base64 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+hf_api_key = os.environ['HF_API_KEY']
+
+
+# In[2]:
+
+
+# Helper function
+import requests, json
+
+#Text-to-image endpoint
+def get_completion(inputs, parameters=None, ENDPOINT_URL=os.environ['HF_API_TTI_BASE']):
+    headers = {
+      "Authorization": f"Bearer {hf_api_key}",
+      "Content-Type": "application/json"
+    }   
+    data = { "inputs": inputs }
+    if parameters is not None:
+        data.update({"parameters": parameters})
+    response = requests.request("POST",
+                                ENDPOINT_URL,
+                                headers=headers,
+                                data=json.dumps(data))
+    return json.loads(response.content.decode("utf-8"))
+
+
+# ## Building an image generation app 
+
+# Here we are going to run `runwayml/stable-diffusion-v1-5` using the `🧨 diffusers` library.
+
+# In[3]:
+
+
+prompt = "a dog in a park"
+
+result = get_completion(prompt)
+IPython.display.HTML(f'<img src="data:image/png;base64,{result}" />')
+
+
+# ## Generating with `gr.Interface()`
+
+# In[4]:
+
+
+import gradio as gr 
+
+#A helper function to convert the PIL image to base64
+#so you can send it to the API
+def base64_to_pil(img_base64):
+    base64_decoded = base64.b64decode(img_base64)
+    byte_stream = io.BytesIO(base64_decoded)
+    pil_image = Image.open(byte_stream)
+    return pil_image
+
+def generate(prompt):
+    output = get_completion(prompt)
+    result_image = base64_to_pil(output)
+    return result_image
+
+gr.close_all()
+demo = gr.Interface(fn=generate,
+                    inputs=[gr.Textbox(label="Your prompt")],
+                    outputs=[gr.Image(label="Result")],
+                    title="Image Generation with Stable Diffusion",
+                    description="Generate any image with Stable Diffusion",
+                    allow_flagging="never",
+                    examples=["the spirit of a tamagotchi wandering in the city of Vienna","a mecha robot in a favela"])
+
+demo.launch(share=True, server_port=int(os.environ['PORT1']))
+
+
+# In[5]:
+
+
+demo.close()
+
